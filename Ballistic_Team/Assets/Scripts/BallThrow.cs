0836130bf1d5450d60052757b70cbe260b10d156
Tiphe0821 <namucho24@gmail.com>
2026-05-13 01:52:42 +0900

    [feat] temp_commit
    생성된 스크립트 임시 저장

---
+++ Ballistic_Team/Assets/Scripts/BallThrow.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BallThrow : MonoBehaviour
+{
+    public GameObject currentBall;
+
+
+    public bool isGrab = false;
+
+    void Start()
+    {
+        
+    }
+
+
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void BallGrab()         // 공 잡기
+    {
+
+    }
+
+    private void BallRelease()      // 공 던지기
+    {
+
+    }
+}
