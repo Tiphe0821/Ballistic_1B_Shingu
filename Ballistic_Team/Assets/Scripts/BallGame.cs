c345b0c04d2870ea67d0c99839cef6a380f7f140
Tiphe0821 <namucho24@gmail.com>
2026-05-12 12:04:56 +0900

    [fix] 공 리소스 설정값 수정 & 머지 로직 수정

--- Ballistic_Team/Assets/Scripts/BallGame.cs
+++ Ballistic_Team/Assets/Scripts/BallGame.cs
@@ -1,13 +1,15 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BallGame : MonoBehaviour
 {
-    public GameObject[] fruitPrefabs;
+    public GameObject[] ballPrefabs;
     public float[] ballSizes = { 0.5f, 0.7f, 0.9f, 1.1f, 1.3f, 1.5f, 1.7f, 1.9f };
 
     public GameObject currentBall;
     public int currentBallType;
 
+    public float ballStartHeight = 6.0f;
     public bool isGameOver = false;
     public Camera mainCamera;
 
@@ -46,8 +48,19 @@ public class BallGame : MonoBehaviour
 
             Vector3 newPosition = currentBall.transform.position;
             newPosition.x = worldPosition.x;
+            newPosition.y = worldPosition.y;
 
-            float halfFruitSize = ballSizes[currentBallType] / 2f;
+            float halfBallSize = ballSizes[currentBallType] / 2f;
+
+            //if (newPosition.x < -gameWidth / 2 + halfBallSize)
+            //{
+            //    newPosition.x = -gameWidth / 2 + halfBallSize;
+            //}
+            //
+            //if (newPosition.x > gameWidth / 2 + halfBallSize)
+            //{
+            //    newPosition.x = gameWidth / 2 + halfBallSize;
+            //}
 
             currentBall.transform.position = newPosition;
         }
@@ -67,11 +80,13 @@ public class BallGame : MonoBehaviour
             Vector3 mousePosition = Input.mousePosition;
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);       // 마우스 2D 위치를 월드 3D 좌표로 변환
 
-            Vector3 spawnPosition = new Vector3(worldPosition.x, worldPosition.y, 0);
+            Vector3 spawnPosition = new Vector3(worldPosition.x, ballStartHeight, 0);
 
-            float halfBallSize = ballSizes[currentBallType] / 2f;
+            float halfFruitSize = ballSizes[currentBallType] / 2f;
 
-            currentBall = Instantiate(fruitPrefabs[currentBallType], spawnPosition, Quaternion.identity);
+            //spawnPosition.x = Mathf.Clamp(spawnPosition.x, -gameWidth / 2 + halfFruitSize, gameWidth / 2 - halfFruitSize);
+
+            currentBall = Instantiate(ballPrefabs[currentBallType], spawnPosition, Quaternion.identity);
             currentBall.transform.localScale = new Vector3(ballSizes[currentBallType], ballSizes[currentBallType], 1);
 
             Rigidbody2D rb = currentBall.GetComponent<Rigidbody2D>();
@@ -92,10 +107,5 @@ public class BallGame : MonoBehaviour
             currentBall = null;
             ballTimer = 1.0f;
         }
-    }
-
-    public void SpawnNextBall(Vector3 position)
-    {
-        
     }
 }
